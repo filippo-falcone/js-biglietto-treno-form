24696b71f6ac80b300a5b4c1ceb4ef5558f1bacf
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-06 18:26:58 +0100

    feat: add reset button & function to click

--- js/script.js
+++ js/script.js
@@ -1,19 +1,12 @@
-/* Cosa ci serve: chilometri e età del passeggero*/
-
-// Quando l'utente clicca sul bottone dobbiamo leggereìne i dati
+/* Funzionalità Biglietto*/
+// Quando l'utente clicca sul bottone calcola dobbiamo leggerne i dati
 const priceBtn = document.querySelector('#price-btn');
 priceBtn.addEventListener('click', function () {
-
     const userKm = parseInt(document.querySelector('#km').value);
     const userAge = parseInt(document.querySelector('#age').value);
-    console.log('km', userKm);
-    console.log('età', userAge);
-
     // - Calcolo il prezzo del biglietto
     const price = 0.21;
     const ticketPrice = userKm * price;
-    console.log('prezzo del biglietto', ticketPrice);
-
     // - Applico lo sconto
     let discount = 0;
     if (userAge < 18) {
@@ -21,13 +14,17 @@ priceBtn.addEventListener('click', function () {
     } else if (userAge > 65) {
         discount = ticketPrice * 40 / 100;
     }
-    console.log('sconto', discount);
-
     // - Calcolo il prezzo finale
     const finalPrice = ticketPrice - discount;
-    console.log('prezzo finale', finalPrice);
-
     // - Stampo il prezzo all'utente
     const userMessage = document.querySelector('#message');
     userMessage.append(finalPrice.toFixed(2) + '€');
+});
+
+// Quando l'utente clicca sul bottone pulisci dobbiamo cancellare tutti i dati inseriti
+const resetBtn = document.querySelector('#reset-btn');
+resetBtn.addEventListener('click', function () {
+    const userKm = document.querySelector('#km').value = '';
+    const userAge = document.querySelector('#age').value = '';
+    const userMessage = document.querySelector('#message').innerHTML = 'Il costo del biglietto è: ';
 });
