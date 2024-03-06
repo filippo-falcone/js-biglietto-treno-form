892c40c3f3c97aaf1c9157f59818bde93352ef72
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-06 17:30:05 +0100

    feat: add total price & print result

--- js/script.js
+++ js/script.js
@@ -6,6 +6,28 @@ priceBtn.addEventListener('click', function () {
 
     const userKm = parseInt(document.querySelector('#km').value);
     const userAge = parseInt(document.querySelector('#age').value);
-    console.log(userKm);
-    console.log(userAge);
+    console.log('km', userKm);
+    console.log('età', userAge);
+
+    // - Calcolo il prezzo del biglietto
+    const price = 0.21;
+    const ticketPrice = userKm * price;
+    console.log('prezzo del biglietto', ticketPrice);
+
+    // - Applico lo sconto
+    let discount = 0;
+    if (userAge < 18) {
+        discount = ticketPrice * 20 / 100;
+    } else if (userAge > 65) {
+        discount = ticketPrice * 40 / 100;
+    }
+    console.log('sconto', discount);
+
+    // - Calcolo il prezzo finale
+    const finalPrice = ticketPrice - discount;
+    console.log('prezzo finale', finalPrice);
+
+    // - Stampo il prezzo all'utente
+    const userMessage = document.querySelector('#message');
+    userMessage.append(finalPrice.toFixed(2) + '€');
 });
